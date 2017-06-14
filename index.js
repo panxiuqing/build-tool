af7ca2e6ba5ed9994d30fca279c9184bbcbfbb0e
pxqing <panxiuqing@gmail.com>
2017-06-15 02:24:21 +0800

    update

--- index.js
+++ index.js
@@ -2,13 +2,23 @@
 const yargs = require('yargs');
 const image2sprites = require('./lib/image2sprites');
 
-argv = yargs.command(
-    'sprites <dir> [output]',
+yargs.usage(
+    '$0 <cmd> [...args]'
+)
+.command(
+    'sprites [input] [output]',
     'generate style file and sprites image from images',
+    {
+        input: {
+            default: './image',
+        },
+        output: {
+            default: './dist'
+        }
+    },
     argv => {
+        image2sprites(argv.input, argv.output);
     }
 )
 .help()
 .argv
-
-image2sprites(argv.dir);
