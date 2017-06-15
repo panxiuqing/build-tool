6e00ca3d1e9fa91ac22bbf70c066094a3af143df
pxqing <panxiuqing@gmail.com>
2017-06-15 14:22:45 +0800

    What:
    Why:
    
    *
    
    How:
    
    *

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const yargs = require('yargs');
 const image2sprites = require('./lib/image2sprites');
+const image2js = require('./lib/image2js');
 
 yargs.usage(
     '$0 <cmd> [...args]'
@@ -18,6 +19,21 @@ yargs.usage(
     },
     argv => {
         image2sprites(argv.input, argv.output);
+    }
+)
+.command(
+    'img2js [input] [output]',
+    'generate es2015 module for [input]/*.png',
+    {
+        input: {
+            default: './image',
+        },
+        output: {
+            default: './dist'
+        }
+    },
+    argv => {
+        image2js(argv.input, argv.output);
     }
 )
 .help()
